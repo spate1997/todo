94fb856649bff3cb63a3480b4c850e2411ba0f60
Sheel <spate022@odu.edu>
2024-03-28 19:20:53 -0400

    first commit

---
+++ testing.rb
@@ -0,0 +1,8 @@
+def display_menu
+    puts "Welcome to the To-Do List!"
+    puts "1. Add a task"
+    puts "2. View tasks"
+    puts "3. Mark a task as completed"
+    puts "4. Exit"
+    print "Enter your choice:"
+end
